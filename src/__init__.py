debe51425ab081006c674236e61c1765ca9a0818
Nikita <zhdan.019@gmail.com>
2025-12-24 10:20:48 +0300

    include ntf to main router

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+from src.notifications.router import router as notifications_router
+
+from fastapi import APIRouter
+
+
+main_router = APIRouter()
+
+main_router.include_router(notifications_router)
+
---
+++ src/main.py
@@ -0,0 +1,15 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from src import main_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    yield
+
+
+app = FastAPI()
+app.include_router(main_router)

b701c83361b7da3e752787d5200e6bdcd5e5f032
Nikita <zhdan.019@gmail.com>
2025-12-24 09:14:20 +0300

    init data base

---
+++ src/database/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from src.core.config import settings
+
+
+engine = create_async_engine(settings.POSTGRES_URL)
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
